42420d5cd6ce3331e12f16d0e35ec065c4301602
jongkyuh <jongkyuh@naver.com>
2023-09-29 12:42:33 +0900

    [1] 초기 서버 구축
    프론트에서 요청한 정보를 백엔드에서 간단한 형태로 리턴해주는 구조
    
    api 통신 가능하게하기(완료)
    포트번호 8080으로 proxy설정(완료)
    프론트서버와 합치기(미완료)

---
+++ src/main/java/com/jobayour/JobayourApplication.java
@@ -0,0 +1,13 @@
+package com.jobayour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobayourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobayourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jobayour/JobayourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobayour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobayourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
