8bdabd86eedb442e1cdf4350f6b0ece70afd68b2
jongkyuh <jongkyuh@naver.com>
2023-10-02 22:39:19 +0900

    [1] 컨트롤러 이름변경

--- src/main/java/com/jobayour/controller/HelloWorldController.java
+++ src/main/java/com/jobayour/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.jobayour.Controller;
+package com.jobayour.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
