bde3ec88c252fad9883e1a458b17fc1ec5066381
jongkyuh <jongkyuh@naver.com>
2023-10-10 22:08:48 +0900

    [3] 프론트에서 받는 4가지 파라미터를 통해 질문을 하는 챗gpt로 로직변경
    1. 직무,연차,난이도,자격요건 DTO 생성
    2. 프론트에서 받는 파라미터를 DTO로 처리
    3. 프론트와 상의하여 통신형태를 변경할지 의논 예정

--- src/main/java/com/jobayour/controller/ChatgptController.java
+++ src/main/java/com/jobayour/controller/ChatgptController.java
@@ -1,14 +1,17 @@
 package com.jobayour.controller;
 
 
+import com.jobayour.dto.QuestionsDTO;
 import com.jobayour.service.ChatService;
-import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,14 +21,17 @@ public class ChatgptController {
 
     private final ChatService chatService;
 
+    //챗gpt 질문 응답 받는 페이지
+    @PostMapping()
+    public List<String> test(@RequestBody QuestionsDTO questionsDTO){
 
+        String combinedQuestions = "직무는" + questionsDTO.getJob() + "이고 연차는 " + questionsDTO.getCareer()
+        + "이고 질문 수준은 최고5라고 하면 " + questionsDTO.getLevel() + "이고 자격요건은 " + questionsDTO.getRequirement() +
+        "일 때 나올 수 있는 면접질문 3가지를 알려줘. 다른말 없이 질문만 알려주고 질문마다 \\로 구분해줘";
+        String response = chatService.getChatResponse(combinedQuestions);
+        List<String> responses = Arrays.asList(response.split("\\."));
 
-
-    //챗gpt
-    @PostMapping("")
-    public String test(@RequestBody String question){
-        return chatService.getChatResponse(question);
-
+        return responses;
 
     }
 
--- src/main/java/com/jobayour/service/ChatService.java
+++ src/main/java/com/jobayour/service/ChatService.java
@@ -4,15 +4,21 @@ import io.github.flashvayne.chatgpt.service.ChatgptService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 @Service
 @RequiredArgsConstructor
 public class ChatService {
+
+
 
     private final ChatgptService chatgptService;     //챗gpt 라이브러리에서 제공하는 클래스
 
     public String getChatResponse(String prompt) {
         // 챗gpt에 질문해서 답변 받기
-        return chatgptService.sendMessage(prompt);
+        String a;
+        a = chatgptService.sendMessage(prompt+"질문마다 마지막에 .을 통해서 구분해주라");
+        return a;
+
     }
 
 }
