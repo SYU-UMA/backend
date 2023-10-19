588a86b291bd03bd3d81f99b0e8bad17e3bc4a18
jongkyuh <jongkyuh@naver.com>
2023-10-19 21:59:30 +0900

    [6] chatgpt응답 컨트롤러,서비스 새로생성(기존컨트롤과 서비스는 일단 유지)
    기존에 프론트에서 직접 받은 데이터를바로 chatgpt api를 통해 검색하는 방법->  db에서 가져온 직업,경력,질문난이도,요구조건을 통해 chatgpt api를 통해 답변을 받음.
    
    답변받은 질문과답변 객체를 다시 저장하는 방법까지는 구현 x

--- src/main/java/com/jobayour/controller/ChatgptController.java
+++ src/main/java/com/jobayour/controller/ChatgptController.java
@@ -22,26 +22,33 @@ import java.util.List;
 @RequestMapping("/chatgpt")                //Chatgpt 연동하는 클래스
 public class ChatgptController {
 
-    private final ChatService chatService;
-
+    private final ChatService chatService;      //chatgpt api통신을 위한 클래스
 
     //챗gpt 질문 응답 받는 페이지
-
     @PostMapping
     public ResponseEntity<List<QuestionAndAnswerDTO>> test(@RequestBody QuestionsDTO questionsDTO) {
+
+        //
+        // quallist db조회해서 필요한 데이터 가져오기 -> 여기부분을 빠르게 만들고
+        //
+
+
+
         String combinedQuestions = "직무는 " + questionsDTO.getJob() + "이고 연차는 " + questionsDTO.getCareer()
                 + "이고 질문 수준은 최고5라고 하면 " + questionsDTO.getLevel() + "이고 자격요건은 " + questionsDTO.getRequirement() +
-                "일 때 나올 수 있는 예상면접질문 5가지와 답변까지 알려줘.  각각의 질문과 답변마다 \\를 꼭 넣어서 구분해줘 그리고 " +
-                "1. 당신의 개발 경력과 관련된 자바 개발 경험은 무엇인가요? \\개발 경력은 5년 이상으로 자바 개발 관련 사업 개발 및 상세 설계, 성능 분석 및 최적화, 시스템 운영과 관련된 경험이 있습니다. " +
+                "일 때 나올 수 있는 예상면접질문 5가지와 답변까지 알려줘.  각 답변마다 \\를 꼭 넣어서 구분해줘 그리고 " +
+                "답변을 1. 당신의 개발 경력과 관련된 자바 개발 경험은 무엇인가요? \\개발 경력은 5년 이상으로 자바 개발 관련 사업 개발 및 상세 설계, " +
+                "성능 분석 및 최적화, 시스템 운영과 관련된 경험이 있습니다. " +
                 "이런식으로 예상질문마다 뒤에는 꼭 ? 로 끝나게해줘 한글로알려줘";
 
         String response = chatService.getChatResponse(combinedQuestions);
 
         List<QuestionAndAnswerDTO> interviewQuestions = new ArrayList<>();
 
-        // \\로 구분하지 않고 직접 객체로 파싱
+        // \로 구분한부분 string 배열로 나누기
         String[] pairs = response.split("\\.");
 
+        // 질문과 답변 나누기
         for (String pair : pairs) {
             String[] parts = pair.split("\\\\");
             if (parts.length >= 2) {
@@ -52,7 +59,12 @@ public class ChatgptController {
             }
         }
 
-        return ResponseEntity.ok(interviewQuestions);
+
+
+        // 로직 돌리고 그다음에 db저장
+
+
+        return ResponseEntity.ok(interviewQuestions);   //프론트에 리턴
     }
 
 }
