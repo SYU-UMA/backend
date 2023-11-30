7210fee933d2719a17e2cb8ec883b30f6ca1f033
jongkyuh <jongkyuh@naver.com>
2023-11-30 19:57:08 +0900

    [12] ChatgptController.java 컨트롤러 수정
    1. ChatgptController 내용의 서비스부분을 분리하기위해 ChatgptSearchService.java 생성
    
    2. 기존에 db에 접근하기위해 단순 String값으로 만든 변수를 jwt토큰에서 가져오는 방식으로 변경
    
    3. 나머지 클래스는 기본 주석 수정 및 삭제 ( 코드 변경 x)

--- src/main/java/com/jobayour/controller/TestChatgptController.java
+++ src/main/java/com/jobayour/controller/TestChatgptController.java
@@ -5,11 +5,10 @@ import com.jobayour.dto.QuestionAndAnswerDTO;
 import com.jobayour.service.GetQualAndChatapiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -26,9 +25,6 @@ public class TestChatgptController {
     //db에서 저장한 데이터를 통해 chatgpt에 검색해서 리턴받는 메소드(
     @PostMapping
     public void test(){
-
-//        기존의 List<QuestionAndAnswerDTO> 으로 리턴할 때의 코드
-//        List<QuestionAndAnswerDTO> interviewQuestions  = getQualAndChatapiService.test();
 
         getQualAndChatapiService.test();
 
---
+++ src/main/java/com/jobayour/service/ChatgptSearchService.java
@@ -0,0 +1,74 @@
+package com.jobayour.service;
+
+import com.jobayour.dto.QuestionAndAnswerDTO;
+import com.jobayour.entity.Interview;
+import com.jobayour.entity.Qualification;
+import com.jobayour.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
+@Service
+@RequiredArgsConstructor
+public class ChatgptSearchService {
+
+    private final InterviewService interviewService;    //db저장을 위한 의존성 주입
+
+    private final ChatService chatService;      //chatgpt api통신을 위한 클래스
+
+    private final QualificationService qualificationService;    //db조회를 위한 위존성 주입
+
+    private final JwtTokenProvider jwtTokenProvider;            //jwt 토큰 정보를 받기위한 Provider
+
+    public void test(HttpServletRequest request){
+
+        int checkNumber = 5;        // 5개의 질문과 답변을 저장하기위해 체크하는 정보
+
+        String token = jwtTokenProvider.resolveToken(request); //HttpServletRequest에서 jwt토큰 추출
+
+        String userId = jwtTokenProvider.getUserPk(token);      //jwt토큰에서 userId추출
+
+        System.out.println(userId);
+
+        // quallist db조회해서 필요한 데이터 가져오기
+        Qualification getData = qualificationService.findTopByIdOrderByQualificationsNumDesc(userId);
+
+
+        while(checkNumber>0){
+
+            String combinedQuestions = "직무는 " + getData.getJob() + "이고 연차는 " + getData.getCareer()
+                    + "이고 질문 수준은 최고5라고 하면 " + getData.getLevel() + "이고 자격요건은 " + getData.getRequirements() +
+                    "일 때 나올 수 있는 예상면접질문 1가지와 답변까지 알려줘.  각 답변마다 \\를 꼭 넣어서 구분해줘 그리고 " +
+                    "답변을 Q. 당신의 개발 경력과 관련된 자바 개발 경험은 무엇인가요? \\A.개발 경력은 5년 이상으로 자바 개발 관련 사업 개발 및 상세 설계, " +
+                    "성능 분석 및 최적화, 시스템 운영과 관련된 경험이 있습니다. " +
+                    "이런식으로 예상질문 뒤에는 꼭 ? 로 끝나게해주고 한글로알려줘";
+
+            String response = chatService.getChatResponse(combinedQuestions);
+
+            String[] pairs = response.split("\\?");
+
+            if(pairs.length >= 2) {
+                String question = pairs[0].trim();
+                String answer = pairs[1].trim();
+                answer = answer.replaceAll("\\\\", "");
+                QuestionAndAnswerDTO questionAndAnswerDTO = new QuestionAndAnswerDTO(question, answer);
+
+                //question과 answer 비어있지 않은지에 대한 여부 확인 후 저장
+                if (questionAndAnswerDTO.isQuestionAndAnswerValid()) {
+
+                    Interview interview = new Interview(getData.getQualificationsNumber()
+                            , "test", questionAndAnswerDTO.getQuestion(), questionAndAnswerDTO.getAnswer());
+
+                    interviewService.addInterview(interview);
+
+                    checkNumber--;
+                }
+            }
+        }
+
+    }
+
+}
