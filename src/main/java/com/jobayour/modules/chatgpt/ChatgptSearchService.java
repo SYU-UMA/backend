abc813b29ba449bfe3f6b72acdab917a1cdc4495
jongkyuh <jongkyuh@naver.com>
2023-12-14 01:13:39 +0900

    [17] 변경된 db로 service 로직 변경
    1. 기존의 qualifications의 내용만 가지고 gpt프롬프트를 만드는 방식에서 사용자의 이력서,스킬,지원서 내용을 추가하여 gpt에 질의하는 방식으로 변경

--- src/main/java/com/jobayour/modules/chatgpt/ChatgptSearchService.java
+++ src/main/java/com/jobayour/modules/chatgpt/ChatgptSearchService.java
@@ -8,6 +8,9 @@ import com.jobayour.modules.qualification.QualificationService;
 import com.jobayour.modules.resumefunc.career.CareerDTO;
 import com.jobayour.modules.resumefunc.career.CareerService;
 
+import com.jobayour.modules.resumefunc.myCareer.MyCareer;
+import com.jobayour.modules.resumefunc.myCareer.MyCareerDTO;
+import com.jobayour.modules.resumefunc.myCareer.MyCareerService;
 import com.jobayour.modules.resumefunc.resumeBasic.ResumeBasic;
 import com.jobayour.modules.resumefunc.resumeBasic.ResumeBasicService;
 import com.jobayour.modules.resumefunc.skill.SkillDTO;
@@ -35,9 +38,16 @@ public class ChatgptSearchService {
     private final ResumeBasicService resumeBasicService;        //resumeBasic 정보 가져오기 위한 서비스
 
     private final SkillService skillService;                    //스킬정보를 가져오기 위한 서비스
-    public void test(HttpServletRequest request,CandidateKeyDTO candidateKeyDTO){       //***프론트에서 resumeNum과 userCareerNum을 매개변수로 받아와야 할듯***
+
+    private final MyCareerService myCareerService;              //지원서 내용 가져오기위한 서비스
+    public void test(HttpServletRequest request,CandidateKeyDTO candidateKeyDTO){
 
         int checkNumber = 5;        // 5개의 질문과 답변을 저장하기위해 체크하는 정보
+
+        String careerPrompt = "";       //career 정보를 담기위한 변수
+        String skillPrompt="";          //skill 정보를 담기위한 변수
+        String myCareerPrompt="";       //myCareer 정보를 담기위한 변수
+        String resumeBasicPrompt="";    //resumeBasic 정보를 담기위한 변수
 
         String token = jwtTokenProvider.resolveToken(request); //HttpServletRequest에서 jwt토큰 추출
 
@@ -47,25 +57,38 @@ public class ChatgptSearchService {
         ResumeBasic resumeBasic1 = new ResumeBasic();
         resumeBasic1.setUserId(userId);
         resumeBasic1.setResumeNum(candidateKeyDTO.getResumeNum());
+
         List<CareerDTO> userCareerList = careerService.findCareerById(resumeBasic1.getUserId(), resumeBasic1.getResumeNum());  //경력서 리스트
 
-        //경력 리스트 여러개 조회되는지 테스트 (회사이름으로 체크)
-        for(int i = 0;i<userCareerList.size();i++){
-            System.out.println("다녀본 회사 : "+userCareerList.get(i).getCompanyName());
+        //경력추가하기
+        for(int i = 0;i< userCareerList.size();i++){
+            careerPrompt+="내가 다녀본 회사는"+userCareerList.get(i).getCompanyName()+",담당한 업무는"
+                    +userCareerList.get(i).getContents()+", 담당부서는"
+                    +userCareerList.get(i).getDeptName()+"이야.";
         }
 
         //스킬 리스트 가져오기
         List<SkillDTO> skillList = skillService.skillListbyIdAndUserId(resumeBasic1.getResumeNum(), resumeBasic1.getUserId());
-
-        //스킬 내용 여러가지 가져와서 체크하기
-        for(int i = 0;i<skillList.size();i++){
-            System.out.println("가지고 있는 skill : " + skillList.get(i).getSkill());
+        //스킬 추가하기
+        String skill="";
+        for(int i = 0;i< skillList.size();i++){
+            skill+=skillList.get(i).getSkill();
         }
+        skillPrompt+="내가 가지고 있는 skill은 " + skill + "이야.";
 
         //이력서 가져오기( My Career 부분 이용!)
-        ResumeBasic userResume = resumeBasicService.findResumeByIdAndnum(userId, candidateKeyDTO.getResumeNum());
-        // 가져온 이력서 내용 체크하기
-        System.out.println(userResume.getResumeName());
+        MyCareerDTO myCareer = myCareerService.findMyCareer(userId, candidateKeyDTO.getResumeNum());
+        //이력서 추가하기
+        myCareerPrompt+="나의 직무는 " + myCareer.getJob() +"이고 나의 소개서는 "
+                + myCareer.getIntroduction() + "이고 내가 가지고 있는 역량은 " + myCareer.getCoreCompetence1()
+                +"," + myCareer.getCoreCompetence1() + "," + myCareer.getCoreCompetence2() + ","
+                + myCareer.getCoreCompetence3() + "," + myCareer.getCoreCompetence4() + ","
+                + myCareer.getCoreCompetence5() +"를 가지고 있어";
+
+        //resumeBasic 내용 가져오기
+        ResumeBasic resumeBasic = resumeBasicService.findResumeByIdAndnum(userId, candidateKeyDTO.getResumeNum());
+        //resumeBasic 내용 추가하기
+        resumeBasicPrompt += "현재 나의 개발자 연차는 " + resumeBasic.getCareer() + "의 연차를 가지고 있어.";
 
 
         // quallist db조회해서 필요한 데이터 가져오기
@@ -77,7 +100,9 @@ public class ChatgptSearchService {
                 "일 때 너가 할 수 있는 예상면접질문 1가지와 답변까지 알려줘.  각 답변마다 \\를 꼭 넣어서 구분해줘 그리고 " +
                 "답변을 Q. 당신의 개발 경력과 관련된 자바 개발 경험은 무엇인가요? \\A.개발 경력은 5년 이상으로 자바 개발 관련 사업 개발 및 상세 설계, " +
                 "성능 분석 및 최적화, 시스템 운영과 관련된 경험이 있습니다. " +
-                "이런식으로 예상질문 뒤에는 꼭 ? 로 끝나게해주고 한글로알려줘";
+                "이런식으로 예상질문 뒤에는 꼭 ? 로 끝나게해주고 한글로알려줘 그리고 현재 나에 대한 정보를 알려줄게. "
+                + careerPrompt +"나가 가진 skill에 대해 알려줄게. " + skillPrompt + " 나의 지원서에 대해 알려줄게." +
+                myCareerPrompt + resumeBasicPrompt +"나의 정보를 가지고 질문과 답변을 알려줘";
 
         while(checkNumber>0){
 
@@ -109,5 +134,7 @@ public class ChatgptSearchService {
         }
 
     }
+
+
 
 }
